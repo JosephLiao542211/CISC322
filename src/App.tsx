4c832435a1815d859fa2cac1bbb226a1374ee9b6
JosephLiao542211 <billan452211@gmail.com>
2024-09-20 16:02:50 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="header">
+        <h1>CISC 322 GROUP 1</h1>
+      </div>
+      <div className="names-list">
+        <h2>Group Members</h2>
+        <ul>
+          <li>Joseph Liao</li>
+          <li>Jane Doe</li>
+          <li>John Smith</li>
+          <li>Emily Johnson</li>
+          <li>Michael Brown</li>
+        </ul>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
