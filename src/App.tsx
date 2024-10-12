a870213b79ef426d5eb8b554f4165eb2444a5777
Joseph Liao <100002111+JosephLiao542211@users.noreply.github.com>
2024-10-12 19:10:23 -0400

    Updated Website with A1 link

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-
-import './App.css'
+import './App.css';
 
 function App() {
- 
-
-  return (
-    <>
-      <div className="header">
-        <h1>DesignMatrix</h1>
-      </div>
-      <div className="names-list">
-        <h2>Group Members</h2>
-        <ul>
-          <li>Joseph Liao</li>
-          <li>Natasha Carneiro</li>
-          <li>Kushaal Dutta</li>
-          <li>James Song</li>
-          <li>Vu Thanh Loc Mai</li>
-          <li>Ahmed Rizwan</li>
-          <li>Adwait Srivastava</li>
-        </ul>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="header">
+                <h1>DesignMatrix</h1>
+            </div>
+            <div className="names-list">
+                <h2>Group Members</h2>
+                <ul>
+                    <li>Joseph Liao</li>
+                    <li>Natasha Carneiro</li>
+                    <li>Kushaal Dutta</li>
+                    <li>James Song</li>
+                    <li>Vu Thanh Loc Mai</li>
+                    <li>Ahmed Rizwan</li>
+                    <li>Adwait Srivastava</li>
+                </ul>
+            </div>
+            <div className="names-list">
+                <h2>Assignment Links</h2>
+                <a href="https://youtu.be/YZdRai4Acd0">A1 Presentation</a>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
