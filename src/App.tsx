6a0f22ffd0652f5d91516fdbf109f89a5be6c699
AdwaitSri <studyuse1702@gmail.com>
2024-10-19 12:10:53 -0400

    Slides and Report update

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,11 @@ function App() {
             </div>
             <div className="names-list">
                 <h2>Assignment Links</h2>
-                <a href="https://youtu.be/YZdRai4Acd0">A1 Presentation</a>
+                <a href="https://youtu.be/YZdRai4Acd0">A1 Presentation Videos</a>
+                <br></br>
+                <a href="https://docs.google.com/document/d/1mo1GT4lq9acY0ROl-6ce3UmdqNgaw4fkyD6yD7HIDYw/edit?usp=sharing">A1 Report </a>
+                <br></br>
+                <a href="https://drive.google.com/file/d/1dgShP_zcq6IYxMj2QtzFW0NzSMQpU8qo/view?usp=sharing">A1 Presentation slides</a>
             </div>
         </>
     );
