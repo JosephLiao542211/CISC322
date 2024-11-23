314ed71fec0b08028500470fc73479611ff83c37
AdwaitSri <studyuse1702@gmail.com>
2024-11-23 12:13:43 -0500

    A2 report

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,28 @@ function App() {
             <div className="header">
                 <h1>DesignMatrix</h1>
             </div>
-            <div className="names-list">
-                <h2>Group Members</h2>
-                <ul>
-                    <li>Joseph Liao</li>
-                    <li>Natasha Carneiro</li>
-                    <li>Kushaal Dutta</li>
-                    <li>James Song</li>
-                    <li>Vu Thanh Loc Mai</li>
-                    <li>Ahmed Rizwan</li>
-                    <li>Adwait Srivastava</li>
-                </ul>
-            </div>
-            <div className="names-list">
-                <h2>Assignment Links</h2>
-                <a href="https://youtu.be/YZdRai4Acd0">A1 Presentation Videos</a>
-                <br></br>
-                <a href="https://docs.google.com/document/d/1mo1GT4lq9acY0ROl-6ce3UmdqNgaw4fkyD6yD7HIDYw/edit?usp=sharing">A1 Report </a>
-                <br></br>
-                <a href="https://drive.google.com/file/d/1dgShP_zcq6IYxMj2QtzFW0NzSMQpU8qo/view?usp=sharing">A1 Presentation slides</a>
+            <div className="container">
+                <div className="card group-members">
+                    <h2>Group Members</h2>
+                    <ul>
+                        <li>Joseph Liao</li>
+                        <li>Natasha Carneiro</li>
+                        <li>Kushaal Dutta</li>
+                        <li>James Song</li>
+                        <li>Vu Thanh Loc Mai</li>
+                        <li>Ahmed Rizwan</li>
+                        <li>Adwait Srivastava</li>
+                    </ul>
+                </div>
+                <div className="card assignment-links">
+                    <h2>Assignment Links</h2>
+                    <a href="https://youtu.be/YZdRai4Acd0">A1 Presentation Videos</a>
+                    <a href="https://docs.google.com/document/d/1mo1GT4lq9acY0ROl-6ce3UmdqNgaw4fkyD6yD7HIDYw/edit?usp=sharing">A1 Report</a>
+                    <a href="https://drive.google.com/file/d/1dgShP_zcq6IYxMj2QtzFW0NzSMQpU8qo/view?usp=sharing">A1 Presentation Slides</a>
+                    <a href="https://drive.google.com/file/d/1ewsV05CXLbGEVvs1pEE5m2kRetZgBHD8/view">A2 Presentation Video</a>
+                    <a href="https://docs.google.com/document/d/10EMA7B5tgYHIZYKPGK92F6dqqlSFAkpLG9jWpTQ4uqw/edit?usp=sharing">A2 Report</a>
+                    <a href="https://drive.google.com/file/d/1kSixV50S-qH7K1cWUT0euui_zDvuattn/view?usp=sharing">A2 Presentation Slides</a>
+                </div>
             </div>
         </>
     );
